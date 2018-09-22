64c7cf37cae134747d3d2aa863fe82545ca1220e
github-xisha <reader_xisha@163.com>
2018-09-23 00:07:51 +0800

    有符号和无符号混合计算

---
+++ 2/2.3.cpp
@@ -0,0 +1,23 @@
+/********************************************************************
+  > File Name: 2.3.cpp
+  > Author:xisha
+  > Mail: 1540348087@qq.com
+  > Created Time: 2018年09月22日 星期六 23时54分04秒
+********************************************************************/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    unsigned int u=10,u2=42;
+    int i=10,i2=42;
+    cout<<"u2-u="<<u2-u<<endl;
+    cout<<"u-u2="<<u-u2<<endl;
+    cout<<"i2-i="<<i2-i<<endl;
+    cout<<"i-i2="<<i-i2<<endl;
+    cout<<"i-u2="<<i-u2<<endl;
+    cout<<"u2-i="<<u2-i<<endl;
+
+}
+
